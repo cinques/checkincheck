0433896c78aea88e1f9d1f7727b7028c8d737597
Pavel Katskov <pasha_kackov@mail.ru>
2018-12-01 15:32:12 +0300

    add correctly paymentService/add_payment request

--- Events/CheckInCheck/RequisitesPicker/RequisitesPicker.js
+++ Events/CheckInCheck/RequisitesPicker/RequisitesPicker.js
@@ -46,9 +46,35 @@ function(CompoundControl, dotTplFn, InformationPopupManager, UserInfo) {
 
         //Вызывается если пользователь проверил и принял указаные реквизиты
         AcceptRequisites: function (){
+            //Получаем данные для добавления платежа и применить!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             var uISF = UserInfo.get('ИдентификаторСервисаПрофилей')
             
-            alert(uISF)
+            //alert(uISF)
+            //Отсылаем запрос на добавление платежей
+            d = uISF
+            
+
+            paymentsData = JSON.stringify({
+                event_id: 'be1e7e45-4081-483c-abae-62105c3f750e',
+                creditor: "b7f8b7cc-1618-43ca-b0a0-454f14a8f147",
+                status: 0,
+                req: '123456789012345',
+                debitor: [
+                    {
+                        debitor: "8a6603e0-f8cc-4528-8a58-6041cf410213",
+                        sum: 500
+                    }
+                ]
+
+                
+            })
+            fetch('/paymentService/add_payment', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: paymentsData
+            });
         }
 
         
